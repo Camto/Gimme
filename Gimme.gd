1b2ab756d07587bfc78dbc027861c4fe8648a622
Camto <www.ben.pr@gmail.com>
2021-04-22 14:01:03 -0400

    Make more car levels.
    Featuring the little ramp :) It's my favorite level so far, really uses the mechanic.

--- Gimme.gd
+++ Gimme.gd
@@ -9,7 +9,7 @@ var Tic_Tac_Toe = preload("res://Tic_Tac_Toe.tscn")
 var X = preload("res://X.tscn")
 var Car = preload("res://Car.tscn")
 
-var level = 1
+var level = 4
 var objects = []
 
 func _ready():
